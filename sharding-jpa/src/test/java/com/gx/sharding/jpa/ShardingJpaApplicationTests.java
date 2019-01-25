a7751a10dea7c8a7aca1da1c78952e6c8711af9d
windlikeman <15068710662@126.com>
2019-01-25 11:04:50 +0800

    fix

--- sharding-jpa/src/test/java/com/gx/sharding/jpa/ShardingJpaApplicationTests.java
+++ sharding-jpa/src/test/java/com/gx/sharding/jpa/ShardingJpaApplicationTests.java
@@ -1,17 +1,18 @@
 package com.gx.sharding.jpa;
 
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class ShardingJpaApplicationTests {
-//
-//    @Test
-//    public void contextLoads() {
-//    }
-//
-//}
-//
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShardingJpaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
+
