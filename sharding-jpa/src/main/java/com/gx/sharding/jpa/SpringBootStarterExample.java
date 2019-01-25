7ea44b18fb3507da33d62d5b37b0fbe09e7a239e
windlikeman <15068710662@126.com>
2019-01-25 10:43:50 +0800

    springboot-jpa

--- sharding-jpa/src/main/java/com/gx/sharding/jpa/SpringBootStarterExample.java
+++ sharding-jpa/src/main/java/com/gx/sharding/jpa/SpringBootStarterExample.java
@@ -1,21 +1,4 @@
-/*
- * Copyright 2016-2018 shardingsphere.io.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * </p>
- */
-
-package io.shardingsphere.example.spring.boot.jpa.orche;
+package com.gx.sharding.jpa;
 
 import io.shardingsphere.example.repository.api.service.CommonService;
 import io.shardingsphere.example.repository.jpa.service.SpringEntityService;
@@ -31,13 +14,13 @@ import org.springframework.context.annotation.ComponentScan;
 @EntityScan(basePackages = "io.shardingsphere.example.repository.jpa.entity")
 @SpringBootApplication(exclude = JtaAutoConfiguration.class)
 public class SpringBootStarterExample {
-    
+
     public static void main(final String[] args) {
         try (ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootStarterExample.class, args)) {
             process(applicationContext);
         }
     }
-    
+
     private static void process(final ConfigurableApplicationContext applicationContext) {
         CommonService commonService = getCommonService(applicationContext);
         commonService.processSuccess(false);
@@ -48,7 +31,7 @@ public class SpringBootStarterExample {
             commonService.printData(false);
         }
     }
-    
+
     private static CommonService getCommonService(final ConfigurableApplicationContext applicationContext) {
         return applicationContext.getBean(SpringEntityService.class);
     }
