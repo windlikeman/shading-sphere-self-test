3c4141daa9fabd2ce2e522bf02c8c69ea984517b
windlikeman <15068710662@126.com>
2019-01-25 10:24:27 +0800

    springboot 2.0.8

--- springboot-mybatis/src/main/java/com/gx/springboot/mybatis/SpringbootMybatisApplication.java
+++ springboot-mybatis/src/main/java/com/gx/springboot/mybatis/SpringbootMybatisApplication.java
@@ -1,21 +1,4 @@
-/*
- * Copyright 2016-2018 shardingsphere.io.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * </p>
- */
-
-package io.shardingsphere.example.spring.boot.mybatis.orche;
+package com.gx.springboot.mybatis;
 
 import io.shardingsphere.example.repository.api.service.CommonService;
 import io.shardingsphere.example.repository.mybatis.service.SpringPojoService;
@@ -29,14 +12,13 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("io.shardingsphere.example.repository.mybatis")
 @MapperScan(basePackages = "io.shardingsphere.example.repository.mybatis.repository")
 @SpringBootApplication(exclude = JtaAutoConfiguration.class)
-public class SpringBootStarterExample {
-    
+public class SpringbootMybatisApplication {
     public static void main(final String[] args) {
-        try (ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootStarterExample.class, args)) {
+        try (ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringbootMybatisApplication.class, args)) {
             process(applicationContext);
         }
     }
-    
+
     private static void process(final ConfigurableApplicationContext applicationContext) {
         CommonService commonService = getCommonService(applicationContext);
         commonService.initEnvironment();
@@ -50,8 +32,9 @@ public class SpringBootStarterExample {
             commonService.cleanEnvironment();
         }
     }
-    
+
     private static CommonService getCommonService(final ConfigurableApplicationContext applicationContext) {
         return applicationContext.getBean(SpringPojoService.class);
     }
 }
+
--- springboot-mybatis/src/main/java/com/gx/springboot/mybatis/SpringbootMybatisTransactionApplication.java
+++ springboot-mybatis/src/main/java/com/gx/springboot/mybatis/SpringbootMybatisTransactionApplication.java
@@ -1,21 +1,4 @@
-/*
- * Copyright 2016-2018 shardingsphere.io.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * </p>
- */
-
-package io.shardingsphere.example.spring.boot.mybatis.orche;
+package com.gx.springboot.mybatis;
 
 import io.shardingsphere.example.repository.api.service.TransactionService;
 import io.shardingsphere.example.repository.mybatis.service.SpringPojoTransactionService;
@@ -29,14 +12,14 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("io.shardingsphere.example.repository.mybatis")
 @MapperScan(basePackages = "io.shardingsphere.example.repository.mybatis.repository")
 @SpringBootApplication
-public class SpringBootStarterTransactionExample {
-    
+public class SpringbootMybatisTransactionApplication {
+
     public static void main(final String[] args) {
-        try (ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootStarterTransactionExample.class, args)) {
+        try (ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringbootMybatisTransactionApplication.class, args)) {
             process(applicationContext);
         }
     }
-    
+
     private static void process(final ConfigurableApplicationContext applicationContext) {
         TransactionService transactionService = getTransactionService(applicationContext);
         transactionService.initEnvironment();
@@ -47,7 +30,7 @@ public class SpringBootStarterTransactionExample {
 //        processFailureSingleTransaction(transactionService, TransactionType.LOCAL);
         transactionService.cleanEnvironment();
     }
-    
+
     private static void processFailureSingleTransaction(final TransactionService transactionService, final TransactionType type) {
         try {
             switch (type) {
@@ -67,8 +50,9 @@ public class SpringBootStarterTransactionExample {
             transactionService.printData(false);
         }
     }
-    
+
     private static TransactionService getTransactionService(final ConfigurableApplicationContext applicationContext) {
         return applicationContext.getBean("jdbcTransactionService", SpringPojoTransactionService.class);
     }
 }
+
